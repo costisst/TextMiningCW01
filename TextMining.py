fbe7bbcbdf201a5ad3ca78fb4c3b2ec2c3bd606c
Konstantinos Stefanopoulos <costisst@hotmail.com>
2020-02-23 01:32:23 +0000

    Add files via upload

--- TextMining.py
+++ TextMining.py
@@ -7,36 +7,13 @@ Created on Sat Feb 15 21:30:25 2020
 
 import string
 import math
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-###############################################################################
-###############################################################################
-###############################################################################
-
-class Network(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-        # Inputs to hidden layer linear transformation
-        self.hidden = nn.Linear(100, 75)
-        # Output layer, 10 units - one for each digit
-        self.output = nn.Linear(75, 50)
-        
-        # Define sigmoid activation and softmax output 
-        self.sigmoid = nn.Sigmoid()
-        self.softmax = nn.Softmax(dim=1)
-        
-    def forward(self, x):
-        # Pass the input tensor through each of our operations
-        x = self.hidden(x)
-        x = self.sigmoid(x)
-        x = self.output(x)
-        x = self.softmax(x)
-        
-        return x
+import nltk
+from nltk.stem import 	WordNetLemmatizer
 
 ###############################################################################
 ###############################################################################
@@ -61,6 +38,7 @@ def create_questions_tag_dict(questions_and_tags):
         bag_of_words = question.split(' ')
         for word in bag_of_words: 
             word = word.strip()
+            word = wordnet_lemmatizer.lemmatize(word)
             if word:
                 if word in my_dictionary:
                     my_dictionary[word] += 1
@@ -94,11 +72,6 @@ def bag_of_words(question,my_dictionary,embedding_maxtrix):
     else:
         bow_vec = (1/len(indexes))*bow_vec
         
-    
-    
-    
-    
-#    context_idxs = torch.tensor(bow_vec, dtype=torch.long)
     return bow_vec
 
 ###############################################################################
@@ -116,17 +89,23 @@ def read_file(path):
 ###############################################################################
 ###############################################################################
 
+
+# Declare Variables
+wordnet_lemmatizer = WordNetLemmatizer()
+#dtype = torch.cuda.FloatTensor # run on GPU
 torch.manual_seed(1)
 tags = []
 questions = []
-k = 2
-D = 100
+k = 3
+D = 300
 
+# Load data 
 stopwords = read_file('D:/Msc_AI_UoM/Semester 2/Text mining/stopwords.txt')
 temp_questions = read_file('D:/Msc_AI_UoM/Semester 2/Text mining/questions.txt')
 temp_questions.pop()
 temp_questions = set(temp_questions)
 
+# Create dictionary based on data
 questions,tags ,my_dictionary = create_questions_tag_dict(temp_questions)
 
 # Remove words with less than k appearences
@@ -148,7 +127,7 @@ my_dictionary = {word: i for i, word in enumerate(vocab)}
 #  N words in vocab, D dimensional embeddings
 embedding_maxtrix = nn.Embedding(N, D)  
 
-
+# Create Training and Testing sets
 training_questions = questions[:math.floor(0.9*len(questions))]
 training_tags = tags[:math.floor(0.9*len(tags))]
 
@@ -156,34 +135,36 @@ testing_questions = questions[math.floor(0.9*len(questions)):]
 testing_tags = tags[math.floor(0.9*len(tags)):]
 
 
-# Produce the input vector
-bow_vec = bag_of_words(questions[0],my_dictionary,embedding_maxtrix)
-
 # Tag dict
 unique_tags = set(tags)
 tag_dict = {word: i for i, word in enumerate(unique_tags)}
 
-
-model = nn.Sequential(nn.Linear(D, 750),
+# Create Neural Network
+model = nn.Sequential(nn.Linear(D, 100),
                       nn.ReLU(), 
-#                      nn.Linear(750, 750),
+#                      nn.Linear(200, 100),
 #                      nn.ReLU(),
-                      nn.Linear(750, 50),
-#                      nn.ReLU(),           
+#                      nn.Linear(500, 250),
+#                      nn.Sigmoid(),
 #                      nn.Linear(100, 50),
+#                      nn.ReLU(),           
+                      nn.Linear(100, 50),
                       nn.LogSoftmax(dim=1))
+
+# Use cuda
+#model.cuda()
+
 # Define the loss
 criterion = nn.NLLLoss()
+
 # Optimizers require the parameters to optimize and a learning rate
-
-
 learning_rate = [0.0001,0.001,0.01,0.1,1,5,10]
-optimizer = optim.Adam(model.parameters(),lr=0.0003)
+optimizer = optim.Adam(model.parameters(), lr=0.00005)
 
 
 test_score = []
 train_score = []
-epochs = 10
+epochs = 50
 for e in range(epochs):
     running_loss = 0
     count_samples = 0
@@ -193,15 +174,16 @@ for e in range(epochs):
     for question, tag in zip(training_questions,training_tags):
         
         count_samples += 1
-        # Flatten MNIST images into a 784 long vector
+        
+        # Produce the input vector
         bow_vec = bag_of_words(question,my_dictionary,embedding_maxtrix)
         
+        # Produce target
         tag_tens = torch.tensor([tag_dict[tag]], dtype=torch.long)
           
         # Training pass
         optimizer.zero_grad()
         output = model(bow_vec.float())
-#        print(output)
         loss = criterion(output, tag_tens)
         
         if torch.eq(tag_tens, torch.exp(output).argmax()):
@@ -212,13 +194,15 @@ for e in range(epochs):
         
         running_loss += loss.item()
     
-    
+    # Testing
     for question, tag in zip(testing_questions,testing_tags):
             
         count_samples_test += 1
-        # Flatten MNIST images into a 784 long vector
+        
+        # Produce the input vector
         bow_vec = bag_of_words(question,my_dictionary,embedding_maxtrix)
         
+        # Produce target
         tag_tens = torch.tensor([tag_dict[tag]], dtype=torch.long)
           
         output = model(bow_vec.float())
@@ -235,11 +219,7 @@ for e in range(epochs):
     print("Epochs: {0}".format(e))    
     print("Training loss: {0}".format(count_correct/count_samples))
     print("Testing loss: {0}".format(count_correct_test/count_samples_test))   
-#        print(f"Training loss: {running_loss/len(questions)}")
-        
-        
-        
-        
+    
         
         
         
